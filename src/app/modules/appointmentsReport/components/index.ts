de27670452516dd061782fe89823293c413edb23
JlaraCast <163551527+JlaraCast@users.noreply.github.com>
2026-06-19 21:13:35 -0600

    feat(appointmentsReport): add FilterPanel, StatsGrid and ResultsCard components

---
+++ src/app/modules/appointmentsReport/components/index.ts
@@ -0,0 +1,3 @@
+export { default as AppointmentsReportFilterPanel } from './AppointmentsReportFilterPanel';
+export { default as AppointmentsReportStatsGrid } from './AppointmentsReportStatsGrid';
+export { default as AppointmentsReportResultsCard } from './AppointmentsReportResultsCard';
